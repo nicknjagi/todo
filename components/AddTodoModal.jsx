a7804bd05e0be22e8ab11df64bd5ea828ad577ce
Nick <njaginicholas@gmail.com>
2024-05-02 18:20:52 +0300

    feat: create AddTodoModal.jsx

---
+++ components/AddTodoModal.jsx
@@ -0,0 +1,36 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  useDisclosure,
+  Button,
+} from "@nextui-org/react";
+import AddForm from "./AddForm";
+
+const AddTodoModal = () => {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  return (
+    <>
+      <Button onPress={onOpen} color="primary">
+        + Add Todo
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Create todo
+              </ModalHeader>
+              <ModalBody>
+                <AddForm onClose={onClose}/>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default AddTodoModal;
