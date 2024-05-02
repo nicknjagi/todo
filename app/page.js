e4afd52e08b54f631ad1c31348bd5a3a1de2868f
Nick <njaginicholas@gmail.com>
2024-05-02 18:20:06 +0300

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,36 +1,29 @@
 'use client'
-import { useQuery } from "@tanstack/react-query";
-import Todo from "@/components/Todo";
 
-const TODOS = [
-  {
-    title:'Build UI for onboarding flow',
-    details:"Make beautiful websites regardless of your design experience. Lorem ipsum dolor sit amet.",
-    priority:'Medium',
-    complete:false,
-    timeTaken:'1hr 15mins',
-    dueDate:'02/04/2024',
-    createdAt:'01/02/2024 at 3:16pm'
-  },
-]
+import { useQuery } from "@tanstack/react-query";
+import { fetchTodos } from "@/api/actions";
+import Todo from "@/components/Todo";
+import AddTodoModal from "@/components/AddTodoModal";
 
 export default function Home() {
   const {data, isLoading, isError, isSuccess} = useQuery({
-    queryKey:["todos"],
-    queryFn: () => wait(1000).then(() => [...TODOS])
+    queryKey: ['todos'],
+    queryFn: fetchTodos
   })
 
   if(isLoading) return <h1 className="text-xl text-center mt-12">Loading...</h1>
 
+  if (isError) {
+    return <div className="text-center mt-12">Error fetching todos</div>;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center gap-6 py-24 px-6">
+      <h1 className="text-xl">Todos</h1>
       {data.map((todo, index)=> {
         return <Todo key={`${todo.title} ${index}`} todo={todo}/>
       })}
+      <AddTodoModal />
     </main>
   );
-}
-
-function wait(duration){
-  return new Promise(resolve => setTimeout(resolve, duration))
 }
