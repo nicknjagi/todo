4e18a6a10b3f84e95839902b1e2e841b2dad2942
Nick <njaginicholas@gmail.com>
2024-05-03 08:08:45 +0300

    update actions.js

--- api/actions.js
+++ api/actions.js
@@ -27,3 +27,45 @@ export async function createTodo(todo){
     throw error; 
   }
 }
+
+export async function updateTodo(todo){
+  const {id} = todo
+  try {
+    const response = await fetch(`http://localhost:8000/todos/${id}`, {
+      method: 'PATCH', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error status: ${response.status}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error updating todo:', error);
+    throw error; 
+  }
+}
+
+export async function deleteTodo(id){
+  try {
+    const response = await fetch(`http://localHost:8000/todos/${id}`, {   
+      method: 'DELETE',
+      headers: {
+          'Content-Type':'application/json'
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error status: ${response.status}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error deleting todo:', error);
+    throw error; 
+  }
+}
